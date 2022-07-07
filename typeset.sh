931adc9715c1396f972f8222b8f14568855d508a
George Chang <59218296+gchang12@users.noreply.github.com>
2022-07-07 08:59:52 -1000

    Update typeset.sh

--- typeset.sh
+++ typeset.sh
@@ -1,16 +1,16 @@
 if [ -n "$1" ]; then
     if [ ! -e "output/$1.pdf" ]; then
     # Append list of comma-separated pairs of page numbers and chapter titles
-        touch {appendix,chapters,front-matter}/$1.csv;
-        vi {appendix,chapters,front-matter}/$1.csv;
+        touch "{appendix,chapters,front-matter}/$1.csv";
+        vi "{appendix,chapters,front-matter}/$1.csv";
     fi;
     # For TeX to process
-    echo $1 > tex/bookname.tex;
-    pdflatex -jobname=$1 main;
+    echo "$1" > tex/bookname.tex;
+    pdflatex -jobname="$1" main;
     # This is where your output will be stored
-    mv $1.pdf output;
+    mv "$1.pdf" output;
     # Cleanup
-    rm $1.*;
+    rm "$1.*";
 else
     # Shows the user the list of possible choices.
     ls src | sed s/.pdf//;
